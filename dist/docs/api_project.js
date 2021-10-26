1b4c007a940927d78edc01cbac653efc664d42b9
BSimardJulien <julien.boisvert-simard.1@ens.etsmtl.ca>
2021-10-26 15:30:36 -0400

    ok

--- dist/docs/api_project.js
+++ dist/docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-16T16:24:10.157Z",
+    "time": "2021-10-26T18:53:26.505Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
